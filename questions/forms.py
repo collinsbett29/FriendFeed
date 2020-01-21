a571b09bc58ee9f8ba0e8ec148ce9d8019f152aa
collinsbett29 <collinsbett29@gmail.com>
2020-01-21 16:32:50 +0300

    Create an answer form

---
+++ questions/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from questions.models import Question, Answer
+from accounts.models import User
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('group', 'name', 'body',)
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ('body', 'question', 'author')
+        widgets = {'question': forms.HiddenInput(
+        ), 'author': forms.HiddenInput()}
