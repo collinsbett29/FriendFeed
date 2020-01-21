b9109931d87a46e6171cb03437d96a420ea93017
collinsbett29 <collinsbett29@gmail.com>
2020-01-21 14:46:22 +0300

    Create a signup form

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+# this returns the user model currently active in this project
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreateForm(UserCreationForm):
+	class Meta():
+		fields = ('username','email','first_name','last_name','password1','password2','bio','image','location')
+		model = get_user_model()
