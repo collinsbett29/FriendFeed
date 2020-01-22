4a705e7b979a0c82a90f04ec86c085cae40cc0bb
collinsbett29 <collinsbett29@gmail.com>
2020-01-22 10:44:51 +0300

    define save functionality

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# if you have space in string it will fill with dashes etc so your browser can read it
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    image = models.ImageField(upload_to='groups/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
